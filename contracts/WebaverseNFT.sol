75f68077b3ee1b3b4fe5cfb3151d6d4bf5ddb66c
Hamza Tariq <hamzatrq116@gmail.com>
2021-10-04 12:13:59 +0500

    Update WebaverseNFT.sol

--- contracts/WebaverseNFT.sol
+++ contracts/WebaverseNFT.sol
@@ -11,39 +11,55 @@ import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
 contract WebaverseNFT is ERC721URIStorage, EIP712 {
     string private constant SIGNING_DOMAIN = "Webaverse-voucher";
     string private constant SIGNATURE_VERSION = "1";
-    
-    // stores the incrementing nonces for the signers to prevent replay attacks
+
+    // mapping to store the incrementing nonces for the signers to prevent replay attacks
     mapping(address => uint256) public nonces;
-    
+
     // mapping to store the URIs of all the NFTs
     mapping(uint256 => string) private _tokenURIs;
 
     constructor()
         ERC721("WebaverseNFT", "WVRS")
-        EIP712(SIGNING_DOMAIN, SIGNATURE_VERSION) {}
+        EIP712(SIGNING_DOMAIN, SIGNATURE_VERSION)
+    {}
 
-    /// @notice Represents an un-minted NFT, which has not yet been recorded into the blockchain. A signed voucher can be redeemed for a real NFT using the redeem function.
+    // Represents a schema to claim an NFT, which has already been minted on blockchain. A signed voucher can be redeemed for a real NFT using the claim function.
     struct NFTVoucher {
-        // @notice The id of the token to be redeemed. Must be unique - if another token with this ID already exists, the redeem function will revert.
+        // The id of the token to be redeemed. Must be unique - if another token with this ID already exists, the claim function will revert.
         uint256 tokenId;
-        // @notice The minimum price (in wei) that the NFT creator is willing to accept for the initial sale of this NFT.
+        // The valid nonce value of the NFT creator, fetched through _nonces mapping.
         uint256 nonce;
-        // @notice The time period for which the voucher is valid
+        // The time period for which the voucher is valid.
         uint256 expiry;
-        // @notice the EIP-712 signature of all other fields in the NFTVoucher struct. For a voucher to be valid, it must be signed by an account with the MINTER_ROLE.
+        // The EIP-712 signature of all other fields in the NFTVoucher struct. For a voucher to be valid, it must be signed by the owner account.
         bytes signature;
     }
 
+    /**
+     * @notice Mints the a single NFT with given parameters.
+     * @param account The address on which the NFT will be minted.
+     * @param tokenId The integer id of the NFT.
+     * @param uri The URL of the NFT.
+     * @notice 'tokenId' must be unique and must not overlap any existing tokenId.
+     * @notice 'uri' should be a metadata json object stored on IPFS or HTTP server.
+     **/
     function mint(
         address account,
         uint256 tokenId,
-        string memory cid
+        string memory uri
     ) public {
+        require(!_exists(tokenId), "Error: Token already exists");
         _mint(account, tokenId);
-        _setURI(tokenId, cid);
+        _setURI(tokenId, uri);
     }
-    
 
+    /**
+     * @notice View function that takes the unique tokenId and returns the uri against it.
+     * @param tokenId The integer id of the NFT.
+     * @dev The token being queried should already exist.
+     * @dev URI of the token should already be set.
+     * @return uri URL of the the NFT against the given 'tokenId'.
+     **/
     function tokenURI(uint256 tokenId)
         public
         view
@@ -52,16 +68,22 @@ contract WebaverseNFT is ERC721URIStorage, EIP712 {
         returns (string memory)
     {
         require(_exists(tokenId), "Query for non existent token");
+        require(
+            bytes(_tokenURIs[tokenId]).length > 0,
+            "Error: URI of this token is not set"
+        );
         return _tokenURIs[tokenId];
-        
     }
 
-    /// @notice Redeems an NFTVoucher for an actual NFT, creating it in the process.
-    /// @param claimer The address of the account which will receive the NFT upon success.
-    /// @param voucher A signed NFTVoucher that describes the NFT to be redeemed.
+    /**
+     * @notice Redeems an NFTVoucher for an actual NFT, authorized by the owner.
+     * @param claimer The address of the account which will receive the NFT upon success.
+     * @param voucher A signed NFTVoucher that describes the NFT to be redeemed.
+     * @dev Verification through ECDSA signature of 'typed' data.
+     * @dev Voucher must contain valid signature, nonce, and expiry.
+     **/
     function claim(address claimer, NFTVoucher calldata voucher)
         public
-        payable
         returns (uint256)
     {
         // make sure signature is valid and get the address of the signer
@@ -71,9 +93,12 @@ contract WebaverseNFT is ERC721URIStorage, EIP712 {
             signer == ownerOf(voucher.tokenId),
             "Authorization failed: Invalid signature"
         );
-        
-        require(block.timestamp <= voucher.expiry, "Voucher has already expired");
-        
+
+        require(
+            block.timestamp <= voucher.expiry,
+            "Voucher has already expired"
+        );
+
         require(voucher.nonce == nonces[signer], "Invalid nonce value");
         // transfer the token to the claimer
         _transfer(signer, claimer, voucher.tokenId);
@@ -81,8 +106,13 @@ contract WebaverseNFT is ERC721URIStorage, EIP712 {
         return voucher.tokenId;
     }
 
-    // Use the mapping _tokenURIs for storing the URIs of NFTs
-    function _setURI(uint256 tokenId, string memory cid) internal virtual {
+    /**
+     * @notice Use the mapping _tokenURIs for storing the URIs of NFTs.
+     * @param tokenId The address of the account which will receive the NFT upon success.
+     * @param uri The URL of the NFT, through which the content of the NFT can be accessed.
+     * @dev Token must be minted before setting the URI.
+     **/
+    function _setURI(uint256 tokenId, string memory uri) internal virtual {
         require(
             _exists(tokenId),
             "Setting URI for non-existent token not allowed"
@@ -91,11 +121,14 @@ contract WebaverseNFT is ERC721URIStorage, EIP712 {
             bytes(_tokenURIs[tokenId]).length == 0,
             "This token's URI already exists"
         );
-        _tokenURIs[tokenId] = cid;
+        _tokenURIs[tokenId] = uri;
     }
 
-    /// @notice Returns a hash of the given NFTVoucher, prepared using EIP712 typed data hashing rules.
-    /// @param voucher An NFTVoucher to hash.
+    /**
+     * @notice Returns a hash of the given NFTVoucher, prepared using EIP712 typed data hashing rules.
+     * @param voucher An NFTVoucher to hash.
+     * @return bytes32 digest of the voucher used for the verification of the signature.
+     **/
     function _hash(NFTVoucher calldata voucher)
         internal
         view
@@ -116,9 +149,10 @@ contract WebaverseNFT is ERC721URIStorage, EIP712 {
             );
     }
 
-    /// @notice Returns the chain id of the current blockchain.
-    /// @dev This is used to workaround an issue with ganache returning different values from the on-chain chainid() function and
-    ///  the eth_chainId RPC method. See https://github.com/protocol/nft-website/issues/121 for context.
+    /**
+     * @notice Using low level assembly call to fetch the chain id of the blockchain.
+     * @return Returns the chain id of the current blockchain.
+     **/
     function getChainID() external view returns (uint256) {
         uint256 id;
         assembly {
@@ -127,9 +161,12 @@ contract WebaverseNFT is ERC721URIStorage, EIP712 {
         return id;
     }
 
-    /// @notice Verifies the signature for a given NFTVoucher, returning the address of the signer.
-    /// @dev Will revert if the signature is invalid. Does not verify that the signer is authorized to mint NFTs.
-    /// @param voucher An NFTVoucher describing an unminted NFT.
+    /**
+     * @notice Verifies the signature for a given NFTVoucher, returning the address of the signer.
+     * @param voucher An NFTVoucher describing an NFT.
+     * @dev Will revert if the signature is invalid. Does not verify that the signer is authorized to mint NFTs.
+     * @return returns the address of the signer on succesful verification.
+     **/
     function _verify(NFTVoucher calldata voucher)
         internal
         view
